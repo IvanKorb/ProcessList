7dd9c6a40924628b5b6460c0dd91d57fc7bdeb70
IvanKorb <kor.ivan.1987@gmail.com>
2017-05-30 15:49:07 +0600

    Add files via upload

---
+++ process/process/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace process
+{
+    partial class frmFormProc
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lvProcList = new System.Windows.Forms.ListView();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.timerRefresh = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lvProcList
+            // 
+            this.lvProcList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvProcList.Location = new System.Drawing.Point(23, 13);
+            this.lvProcList.Name = "lvProcList";
+            this.lvProcList.Size = new System.Drawing.Size(509, 187);
+            this.lvProcList.TabIndex = 0;
+            this.lvProcList.UseCompatibleStateImageBehavior = false;
+            this.lvProcList.View = System.Windows.Forms.View.Details;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnRefresh.Location = new System.Drawing.Point(23, 227);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(87, 29);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "&Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnClose.Location = new System.Drawing.Point(129, 227);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(83, 29);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // timerRefresh
+            // 
+            this.timerRefresh.Enabled = true;
+            this.timerRefresh.Interval = 3000;
+            this.timerRefresh.Tick += new System.EventHandler(this.timerRefresh_Tick);
+            // 
+            // frmFormProc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 311);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.lvProcList);
+            this.Name = "frmFormProc";
+            this.Text = "Process List View";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvProcList;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Timer timerRefresh;
+    }
+}
+
---
+++ process/process/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace process
+{
+    public partial class frmFormProc : Form
+    {
+        AutoResetEvent EventRefresh;
+        public frmFormProc()
+        {
+            InitializeComponent();
+            EventRefresh = new AutoResetEvent(true);
+            Thread ProcThread = new Thread(ProcessListThread);
+            ProcThread.IsBackground = true;
+            ProcThread.Start(EventRefresh);
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            UpdateProcessList();
+
+        }
+
+        void UpdateProcessList()
+        {
+            EventRefresh.Set();
+        }
+
+        void ProcessListThread(object param)
+        {
+            //для не пересечения с индексом другого потока
+            Thread.Sleep(100);
+            AutoResetEvent eventRefresh = (AutoResetEvent)param;
+
+            Invoke((Action)delegate
+            {
+                lvProcList.Columns.Clear();
+                lvProcList.Columns.Add("Process Name");
+                lvProcList.Columns.Add("Id");
+                lvProcList.Columns.Add("Count Threads");
+                lvProcList.Columns.Add("Count Descrypt");
+                // lvProcList.Columns.Add("Module Name");
+                timerRefresh.Stop();
+            });
+            while (true)
+            {
+                eventRefresh.WaitOne();
+                Process[] proc = Process.GetProcesses();
+                Invoke((Action)delegate
+                {
+                    lvProcList.Items.Clear();
+                    //чтоб не дергался list view
+                    lvProcList.BeginUpdate();
+                    foreach (Process p in proc)
+                    {
+                        //фоормирование строки для list view
+                        //Добавление корневого элемента
+                        ListViewItem lvl = new ListViewItem(p.ProcessName);
+
+                        //Добавление следующих элементво в колонки
+                        lvl.SubItems.Add(p.Id.ToString());
+                        lvl.SubItems.Add(p.Threads.Count.ToString());
+                        lvl.SubItems.Add(p.HandleCount.ToString());
+                        //lvl.SubItems.Add(p.Modules[0].FileName);
+
+
+                        lvProcList.Items.Add(lvl);
+
+                    }
+                    lvProcList.EndUpdate();
+                });
+            }
+        }
+
+        private void timerRefresh_Tick(object sender, EventArgs e)
+        {
+            UpdateProcessList();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
